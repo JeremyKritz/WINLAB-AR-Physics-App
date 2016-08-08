f5ecf7c29eec83143dacedff8ca8c2a9dbadab2b
brand <hiiiii>
2016-08-08 15:12:19 -0400

    sup

---
+++ Assets/GravityControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityControl : MonoBehaviour
+{
+
+    public float gravityControl = -9.81f;
+
+    void Start()
+    {
+        Physics.gravity = new Vector3(0, gravityControl, 0);
+
+    }
+
+    void Update()
+    {
+        Physics.gravity = new Vector3(0, gravityControl, 0);
+
+    }
+    public void changeGravity(float newGravity)
+    {
+        gravityControl = newGravity;
+    }
+
+
+}
+
---
+++ Assets/GravityReader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GravityReader : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text gravity = null;
+    private bool alpha = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 gravityHere = Physics.gravity;
+        gravity.text = "Gravity is: " + -9.81;
+    }
+    void Update()
+    {
+        Vector3 gravityHere = Physics.gravity;
+        gravity.text = "Gravity is: " + gravityHere.y;
+    }
+}
