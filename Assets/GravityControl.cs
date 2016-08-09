35e776a7b00057fb0591b098cccd5fdc5f086b50
brand <hiiiii>
2016-08-09 14:28:24 -0400

    added hands, before other stuff

--- Assets/GravityControl.cs
+++ Assets/GravityControl.cs
@@ -7,84 +7,65 @@ public class GravityControl : MonoBehaviour
     public float gravityControl = -9.81f;
     private GameObject planet;
 
-
-
-
-
-
-    void Start()
-    {
+    void Start() {
         Physics.gravity = new Vector3(0, gravityControl, 0);
     }
 
-    void Update()
-    {
+    void Update(){
         Physics.gravity = new Vector3(0, gravityControl, 0);
     }
-    public void changeGravity(float newGravity)
-    {
-        gravityControl = newGravity;
-       
-            
+    public void changeGravity(float newGravity){
+        gravityControl = newGravity;   
     }
-    public void changeGravityNeptune()
-    {
-        gravityControl = (float)-11.15;
+    public void changeGravityNeptune() { 
+        Physics.gravity = new Vector3(0, (float)-11.15, 0);
         planet = GameObject.Find("Planet");
         planet.GetComponent<Renderer>().material.mainTexture=Resources.Load("neptunemap") as Texture;
     }
-    public void changeGravitySaturn()
-    {
-        gravityControl = (float)-10.44;
+    public void changeGravitySaturn(){
+        Physics.gravity = new Vector3(0, (float)-10.44, 0);
         planet = GameObject.Find("Planet");
         planet.GetComponent<Renderer>().material.mainTexture = Resources.Load("saturnmap") as Texture;
     }
-    public void changeGravityEarth()
-    {
-        gravityControl = (float)-9.81;
+    public void changeGravityEarth(){
+        Physics.gravity = new Vector3(0, (float)-9.81, 0);
         planet = GameObject.Find("Planet");
         planet.GetComponent<Renderer>().material.mainTexture = Resources.Load("earthmap1k") as Texture;
     }
-    public void changeGravityMercury()
-    {
-        gravityControl = (float)-3.7;
+    public void changeGravityMercury() { 
+        Physics.gravity = new Vector3(0, (float)-3.7, 0);
         planet = GameObject.Find("Planet");
         planet.GetComponent<Renderer>().material.mainTexture = Resources.Load("mercurymap") as Texture;
     }
-    public void changeGravityMoon()
-    {
-        gravityControl = (float)-1.62;
+    public void changeGravityMoon() { 
+        Physics.gravity = new Vector3(0, (float) - 1.62, 0);
         planet = GameObject.Find("Planet");
         planet.GetComponent<Renderer>().material.mainTexture = Resources.Load("moonmap1k") as Texture;
     }
-    public void changeGravityPluto()
-    {
-        gravityControl = (float)-0.58;
+    public void changeGravityPluto(){
+        Physics.gravity = new Vector3(0, (float)-0.58, 0); 
         planet = GameObject.Find("Planet");
         planet.GetComponent<Renderer>().material.mainTexture = Resources.Load("plutomap1k") as Texture;
     }
 
-    public void changeGravityVenus()
-    {
-        gravityControl = (float)-8.87;
+    public void changeGravityVenus() { 
+        Physics.gravity = new Vector3(0, (float)-8.87, 0);
         planet = GameObject.Find("Planet");
         planet.GetComponent<Renderer>().material.mainTexture = Resources.Load("venusmap") as Texture;
     }
-    public void changeGravityUranus()
-    {
-        gravityControl = (float)-8.87;
+    public void changeGravityUranus() { 
+        Physics.gravity = new Vector3(0, (float)-8.87, 0);
         planet = GameObject.Find("Planet");
         planet.GetComponent<Renderer>().material.mainTexture = Resources.Load("uranusmap") as Texture;
     }
-    public void changeGravityJupiter()
-    {
-        gravityControl = (float)-24.92;
+    public void changeGravityJupiter() {
+        Physics.gravity = new Vector3(0, (float)-24.92, 0);
         planet = GameObject.Find("Planet");
         planet.GetComponent<Renderer>().material.mainTexture = Resources.Load("jupitermap") as Texture;
     }
     public void changeGravityNoGravity()
     {
-        gravityControl = (float)0;
+        Physics.gravity = new Vector3(0, 0, 0);
     }
 
 
--- Assets/SliderChange.cs
+++ Assets/SliderChange.cs
@@ -6,11 +6,18 @@ public class SliderChange : MonoBehaviour
 {
     public Slider mainSlider;
 
+    int y = 0;
     //Invoked when a submit button is clicked.
     public void Update()
     {
-        //Displays the value of the slider in the console.
-        Vector3 sup = Physics.gravity;
-        mainSlider.value = sup.y;
+        if (y == 60) { 
+            //Displays the value of the slider in the console.
+            Vector3 sup = Physics.gravity;
+            mainSlider.value = sup.y;
+            y = 0;
+        }else
+        {
+            y++;
+        }
     }
 }
