b8242a6824691be93367c41021b4e3fd3d12f832
bred <dddd>
2016-08-11 11:53:52 -0400

    reset positin not working

--- Assets/createObjects.cs
+++ Assets/createObjects.cs
@@ -5,6 +5,8 @@ public class createObjects : MonoBehaviour {
 
     public GameObject createBaseBall;
     public GameObject createBasketball;
+    public GameObject createTennisBall;
+    public GameObject createVolleyBall;
 
     public void createBaseball()
 
@@ -17,8 +19,27 @@ public class createObjects : MonoBehaviour {
     {
         GameObject go = Instantiate(createBasketball, new Vector3(1.08f, 2.5f, -0.48f), Quaternion.identity) as GameObject;
     }
+    public void createTennisbball()
+
+    {
+        GameObject go = Instantiate(createTennisBall, new Vector3(1.08f, 2.5f, -0.48f), Quaternion.identity) as GameObject;
+    }
+
+    public void createVolleyball()
+
+    {
+        GameObject go = Instantiate(createVolleyBall, new Vector3(1.08f, 2.5f, -0.48f), Quaternion.identity) as GameObject;
+    }
 
 
+
+    public void resetPositionofPlanet()
+
+    {
+        GameObject planet;
+        planet = GameObject.Find("Planet");
+        planet.transform.localPosition = new Vector3(.25f, 1.398f, -0.76f);
+    }
     // Update is called once per frame
     void Update () {
 	
